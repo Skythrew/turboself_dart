662cef83c13962ec3a30e4500e120ca21d36ec3b
Maël GUERIN <mael.guerin@murena.io>
2024-12-02 19:42:22 +0100

    feat(models): add `Payment`

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -3,3 +3,4 @@ export 'balance.dart';
 export 'booking.dart';
 export 'booking_day.dart';
 export 'history_event.dart';
+export 'payment.dart';
---
+++ lib/src/models/payment.dart
@@ -0,0 +1,25 @@
+class Payment {
+  final num? id;
+  final num amount;
+  final String status;
+  final String token;
+  final String? url;
+  final String cancelURL;
+  final String returnURL;
+  final DateTime date;
+
+  factory Payment.fromJSON(Map<String, dynamic> json) {
+    return Payment(
+      json['id'],
+      json['montant'],
+      json['statut'],
+      json['token'],
+      null,
+      'https://espacenumerique.turbo-self.com/PagePaiementRefuse.aspx?token=${json['token']}',
+      'https://espacenumerique.turbo-self.com/PagePaiementValide.aspx?token=${json['token']}',
+      DateTime.parse(json['date'])
+    );
+  }
+
+  Payment(this.id, this.amount, this.status, this.token, this.url, this.cancelURL, this.returnURL, this.date);
+}
